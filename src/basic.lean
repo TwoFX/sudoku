a610e27aefc7e8e9dd5109b73316b1f2ac55cfce
Markus Himmel <markus@himmel-villmar.de>
2020-07-14 16:46:06 +0200

    Initial commit

---
+++ src/basic.lean
@@ -0,0 +1,133 @@
+import data.set.function
+import tactic
+
+open set
+
+def row (i : fin 9) : set (fin 9 × fin 9) := { p | p.1 = i }
+def col (i : fin 9) : set (fin 9 × fin 9) := { p | p.2 = i }
+def box (i j : fin 3) : set (fin 9 × fin 9) := { p | p.1.1 / 3 = i.1 ∧ p.2.1 / 3 = j.1 }
+
+lemma mem_row (i j k : fin 9) : (j, k) ∈ row i ↔ j = i := iff.rfl
+lemma mem_col (i j k : fin 9) : (j, k) ∈ col i ↔ k = i := iff.rfl
+lemma mem_box (i j : fin 9) (k l : fin 3) : (i, j) ∈ box k l ↔ i.1 / 3 = k.1 ∧ j.1 / 3 = l.1 := iff.rfl
+
+structure sudoku :=
+(f : fin 9 × fin 9 → fin 9)
+(h_row : ∀ i : fin 9, set.bij_on f (row i) set.univ)
+(h_col : ∀ i : fin 9, set.bij_on f (col i) set.univ)
+(h_box : ∀ i j : fin 3, set.bij_on f (box i j) set.univ)
+
+namespace sudoku
+
+lemma cell_cases (s : sudoku) (i j : fin 9) :
+  s.f (i, j) = 9 ∨ s.f (i, j) = 1 ∨ s.f (i, j) = 2 ∨ s.f (i, j) = 3 ∨ s.f (i, j) = 4 ∨ s.f (i, j) = 5 ∨ s.f (i, j) = 6 ∨ s.f (i, j) = 7 ∨ s.f (i, j) = 8 :=
+begin
+  cases s.f (i, j) with v hv,
+  interval_cases v; tauto
+end
+
+lemma row_cases (s : sudoku) (i j : fin 9) :
+  s.f (i, 0) = j ∨ s.f (i, 1) = j ∨ s.f (i, 2) = j ∨ s.f (i, 3) = j ∨ s.f (i, 4) = j ∨ s.f (i, 5) = j ∨ s.f (i, 6) = j ∨ s.f (i, 7) = j ∨ s.f (i, 8) = j :=
+begin
+  obtain ⟨⟨x, ⟨y, hy⟩⟩, ⟨h, h'⟩⟩ : j ∈ s.f '' row i := (s.h_row i).surj_on trivial,
+  rw mem_row at h,
+  subst h,
+  interval_cases y; tauto
+end
+
+lemma col_cases (s : sudoku) (i j : fin 9) :
+  s.f (0, i) = j ∨ s.f (1, i) = j ∨ s.f (2, i) = j ∨ s.f (3, i) = j ∨ s.f (4, i) = j ∨ s.f (5, i) = j ∨ s.f (6, i) = j ∨ s.f (7, i) = j ∨ s.f (8, i) = j :=
+begin
+  obtain ⟨⟨⟨x, hx⟩, y⟩, ⟨h, h'⟩⟩ : j ∈ s.f '' col i := (s.h_col i).surj_on trivial,
+  rw mem_col at h,
+  subst h,
+  interval_cases x; tauto
+end
+
+lemma box_cases (s : sudoku) (i j : fin 3) (k : fin 9) :
+  s.f ((3 * i.1 : ℕ), (3 * j.1 : ℕ)) = k ∨
+  s.f ((3 * i.1 : ℕ), (3 * j.1 + 1 : ℕ)) = k ∨
+  s.f ((3 * i.1 : ℕ), (3 * j.1 + 2 : ℕ)) = k ∨
+  s.f ((3 * i.1 + 1 : ℕ), (3 * j.1 : ℕ)) = k ∨
+  s.f ((3 * i.1 + 1 : ℕ), (3 * j.1 + 1 : ℕ)) = k ∨
+  s.f ((3 * i.1 + 1 : ℕ), (3 * j.1 + 2 : ℕ)) = k ∨
+  s.f ((3 * i.1 + 2 : ℕ), (3 * j.1 : ℕ)) = k ∨
+  s.f ((3 * i.1 + 2 : ℕ), (3 * j.1 + 1 : ℕ)) = k ∨
+  s.f ((3 * i.1 + 2 : ℕ), (3 * j.1 + 2 : ℕ)) = k :=
+begin
+  obtain ⟨⟨x, y⟩, ⟨h, h'⟩⟩ : k ∈ s.f '' box i j := (s.h_box i j).surj_on trivial,
+  rw mem_box at h,
+  cases h with h₀ h₁,
+  have hx : x.1 = 3 * i.val + (x.1 % 3),
+  { rw [add_comm, ←h₀, nat.mod_add_div] },
+  have hy : y.1 = 3 * j.val + (y.1 % 3),
+  { rw [add_comm, ←h₁, nat.mod_add_div] },
+  have := nat.mod_lt x.val (show 3 > 0, from dec_trivial),
+  have := nat.mod_lt y.val (show 3 > 0, from dec_trivial),
+  interval_cases (x.val % 3);
+  rw h at hx;
+  try { rw add_zero at hx };
+  rw ←hx;
+  interval_cases (y.val % 3);
+  rw h_1 at hy;
+  try { rw add_zero at hy };
+  simp only [←hy, fin.coe_val_eq_self];
+  tauto
+end
+
+lemma cell_conflict (s : sudoku) {i j k l : fin 9} (h₀ : s.f (i, j) = k) (h₁ : s.f (i, j) = l)
+  (h₂ : k ≠ l) : false :=
+begin
+  apply h₂,
+  rw [←h₀, ←h₁]
+end
+
+lemma row_conflict (s : sudoku) {i j k l : fin 9} (h₀ : s.f (i, j) = l) (h₁ : s.f (i, k) = l)
+  (h₂ : j ≠ k) : false :=
+begin
+  apply h₂,
+  suffices : (i, j) = (i, k),
+  { cases this, refl },
+  refine (s.h_row i).inj_on _ _ (h₀.trans h₁.symm);
+  rw mem_row
+end
+
+lemma col_conflict (s : sudoku) {i j k l : fin 9} (h₀ : s.f (i, k) = l) (h₁ : s.f (j, k) = l)
+  (h₂ : i ≠ j) : false :=
+begin
+  apply h₂,
+  suffices : (i, k) = (j, k),
+  { cases this, refl },
+  refine (s.h_col k).inj_on _ _ (h₀.trans h₁.symm);
+  rw mem_col
+end
+
+lemma bloop {i : ℕ} (hi : i < 9) : i / 3 < 3 :=
+by interval_cases i; exact dec_trivial
+
+lemma box_conflict (s : sudoku) {i j k l m : fin 9} (h₀ : s.f (i, j) = m) (h₁ : s.f (k, l) = m)
+  (h₂ : i.1 / 3 = k.1 / 3) (h₃ : j.1 / 3 = l.1 / 3) (h₄ : i ≠ k ∨ j ≠ l) : false :=
+begin
+  contrapose h₄,
+  push_neg,
+  clear h₄,
+  suffices : (i, j) = (k, l),
+  { cases this, exact ⟨rfl, rfl⟩ },
+  refine (s.h_box (i.1 / 3 : ℕ) (j.1 / 3 : ℕ)).inj_on _ _ (h₀.trans h₁.symm),
+  { rw mem_box,
+    split,
+    { rw fin.coe_val_of_lt,
+      exact bloop i.2 },
+    { rw fin.coe_val_of_lt,
+      exact bloop j.2 } },
+  { rw mem_box,
+    split,
+    { rw fin.coe_val_of_lt,
+      { exact h₂.symm },
+      { exact bloop i.2 } },
+    { rw fin.coe_val_of_lt,
+      { exact h₃.symm },
+      { exact bloop j.2 } } }
+end
+
+end sudoku
---
+++ src/stactic.lean
@@ -0,0 +1,202 @@
+import basic
+
+universes u v w
+
+open tactic
+
+def list.mfirst' {m : Type → Type v} [monad m] {α : Type} [inhabited α] (f : α → m bool) : list α → m α
+| [] := return $ default _
+| (a::as) := do
+  r ← f a,
+  if r then return a else list.mfirst' as
+
+def list.mfiltermap {m : Type u → Type v} [monad m] {α : Type w} {β : Type u}
+  (f : α → m (option β)) : list α → m (list β)
+| [] := return []
+| (x::xs) := do
+    y ← f x,
+    r ← list.mfiltermap xs,
+    match y with
+    | none := return r
+    | some z := return (z::r)
+    end
+
+namespace tactic.sudoku
+
+meta def get_sudoku : tactic expr :=
+local_context >>= list.mfirst' (λ t, (do `(sudoku) ← infer_type t, return tt) <|> return ff)
+
+def indices : list (fin 9) := [0, 1, 2, 3, 4, 5, 6, 7, 8]
+def numbers : list (fin 9) := [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+meta structure cell_data :=
+(row col val : fin 9)
+(e : expr)
+
+meta instance format_cell_data : has_to_format cell_data :=
+{ to_format := λ d,
+  format!"{cell_data.e d}" }
+
+meta def parse_cell_data (s e : expr) : tactic (option cell_data) :=
+(do
+  `(sudoku.f %%s (%%a, %%b) = %%c) ← infer_type e,
+  u ← eval_expr (fin 9) a,
+  v ← eval_expr (fin 9) b,
+  w ← eval_expr (fin 9) c,
+  return $ some ⟨u, v, w, e⟩) <|> return none
+
+meta def get_cell_data (s : expr) : tactic (list cell_data) :=
+local_context >>= list.mfiltermap (parse_cell_data s)
+
+meta def mk_row_conflict (s : expr) (l r : cell_data) : tactic unit :=
+do
+  guard (l.row = r.row),
+  guard (l.val = r.val),
+  guard (l.col ≠ r.col),
+  to_expr ``(sudoku.row_conflict %%s %%l.e %%r.e dec_trivial) >>= exact
+
+meta def mk_col_conflict (s : expr) (l r : cell_data) : tactic unit :=
+do
+  guard (l.row ≠ r.row),
+  guard (l.val = r.val),
+  guard (l.col = r.col),
+  to_expr ``(sudoku.col_conflict %%s %%l.e %%r.e dec_trivial) >>= exact
+
+meta def mk_cell_conflict (s : expr) (l r : cell_data) : tactic unit :=
+do
+  guard (l.row = r.row),
+  guard (l.col = r.col),
+  guard (l.val ≠ r.val),
+  to_expr ``(sudoku.cell_conflict %%s %%l.e %%r.e dec_trivial) >>= exact
+
+meta def mk_box_conflict (s : expr) (l r : cell_data) : tactic unit :=
+do
+  guard (l.val = r.val),
+  guard (l.row / 3 = r.row / 3),
+  guard (l.col / 3 = r.col / 3),
+  guard (l.row ≠ r.row ∨ l.col ≠ r.col),
+  to_expr ``(sudoku.box_conflict %%s %%l.e %%r.e dec_trivial dec_trivial dec_trivial) >>= exact
+
+meta def loop (cd : list cell_data) (f : cell_data → cell_data → tactic unit) : tactic unit :=
+list.mfirst (λ l : cell_data, list.mfirst (λ r : cell_data, f l r) cd) cd
+
+meta def row_conflict (s : expr) (cd : list cell_data) : tactic unit :=
+loop cd $ mk_row_conflict s
+
+meta def col_conflict (s : expr) (cd : list cell_data) : tactic unit :=
+loop cd $ mk_col_conflict s
+
+meta def cell_conflict (s : expr) (cd : list cell_data) : tactic unit :=
+loop cd $ mk_cell_conflict s
+
+meta def box_conflict (s : expr) (cd : list cell_data) : tactic unit :=
+loop cd $ mk_box_conflict s
+
+meta def box_logic (r c v : pexpr) : tactic unit :=
+do
+  s ← get_sudoku,
+  h ← to_expr ``(sudoku.box_cases %%s %%r %%c %%v),
+  n ← mk_fresh_name,
+  note n none h,
+  tactic.repeat (auto_cases >> skip)
+
+meta def row_logic (r v : pexpr) : tactic unit :=
+do
+  s ← get_sudoku,
+  h ← to_expr ``(sudoku.row_cases %%s %%r %%v),
+  n ← mk_fresh_name,
+  note n none h,
+  tactic.repeat (auto_cases >> skip)
+
+meta def col_logic (c v : pexpr) : tactic unit :=
+do
+  s ← get_sudoku,
+  h ← to_expr ``(sudoku.col_cases %%s %%c %%v),
+  n ← mk_fresh_name,
+  note n none h,
+  tactic.repeat (auto_cases >> skip)
+
+meta def cell_logic (r c : pexpr) : tactic unit :=
+do
+  s ← get_sudoku,
+  h ← to_expr ``(sudoku.cell_cases %%s %%r %%c),
+  n ← mk_fresh_name,
+  note n none h,
+  tactic.repeat (auto_cases >> skip)
+
+meta def conflict' (s : expr) (cd : list cell_data) : tactic unit :=
+row_conflict s cd <|> col_conflict s cd <|> cell_conflict s cd <|> box_conflict s cd
+
+meta def conflict_with (s : expr) (cd : list cell_data) (e : expr) : tactic unit :=
+do
+  some d ← parse_cell_data s e,
+  conflict' s (d::cd)
+
+meta def conflict : tactic unit :=
+do
+  s ← get_sudoku,
+  cd ← get_cell_data s,
+  conflict' s cd
+
+end tactic.sudoku
+
+namespace tactic.interactive
+
+open tactic.sudoku
+
+setup_tactic_parser
+
+meta def conflict : tactic unit :=
+tactic.sudoku.conflict
+
+meta def box_logic' (r c v : parse parser.pexpr) : tactic unit :=
+tactic.sudoku.box_logic r c v
+
+meta def row_logic' (r v : parse parser.pexpr) : tactic unit :=
+tactic.sudoku.row_logic r v
+
+meta def col_logic' (c v : parse parser.pexpr) : tactic unit :=
+tactic.sudoku.col_logic c v
+
+meta def cell_logic' (r c : parse parser.pexpr) : tactic unit :=
+tactic.sudoku.cell_logic r c
+
+
+meta def all_conflict : tactic unit :=
+do
+  s ← get_sudoku,
+  cd ← get_cell_data s,
+  tactic.all_goals (tactic.try (do
+    e ← tactic.get_local `h,
+    exfalso >> tactic.sudoku.conflict_with s cd e <|> tactic.exact e
+  )) >> skip
+
+meta def box_logic : tactic unit :=
+do
+  `(sudoku.f %%s (%%r, %%c) = %%v) ← target,
+  tactic.sudoku.box_logic ``(((%%r).1 / 3 : ℕ)) ``(((%%c).1 / 3 : ℕ)) (pexpr.of_expr v),
+  all_conflict
+
+meta def row_logic : tactic unit :=
+do
+  `(sudoku.f %%s (%%r, %%c) = %%v) ← target,
+  tactic.sudoku.row_logic (pexpr.of_expr r) (pexpr.of_expr v),
+  all_conflict
+
+meta def col_logic : tactic unit :=
+do
+  `(sudoku.f %%s (%%r, %%c) = %%v) ← target,
+  tactic.sudoku.col_logic (pexpr.of_expr c) (pexpr.of_expr v),
+  all_conflict
+
+meta def cell_logic : tactic unit :=
+do
+  `(sudoku.f %%s (%%r, %%c) = %%v) ← target,
+  tactic.sudoku.cell_logic (pexpr.of_expr r) (pexpr.of_expr c),
+  all_conflict
+
+meta def naked_single : tactic unit :=
+cell_logic
+
+
+end tactic.interactive
