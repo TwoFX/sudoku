3b0e99fa373d2b0d1e0a32f6ae9b05a8bef46cdc
Markus Himmel <markus@himmel-villmar.de>
2020-07-15 13:57:21 +0200

    Rework generator

--- scripts/gen.cpp
+++ scripts/gen.cpp
@@ -25,18 +25,53 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
+bool solve(vector<vector<int>> &s, int i, int j) {
+	if (j == 9) { ++i; j = 0; }
+	if (i == 9) return true;
+
+	if (s[i][j] != 0) return solve(s, i, j + 1);
+
+	forb(k, 9) {
+		bool ok = true;
+		fora(l, 9) if (s[i][l] == k) ok = false;
+		fora(l, 9) if (s[l][j] == k) ok = false;
+		fora(l, 3) fora(m, 3) {
+			if (s[3 * (i / 3) + l][3 * (j / 3) + m] == k) ok = false;
+		}
+		if (!ok) continue;
+		s[i][j] = k;
+		if (solve(s, i, j + 1)) return true;
+	}
+
+	return false;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	fora(i, 9) fora(j, 9) {
-		int k;
-		cin >> k;
-		if (k != 0) {
-			cout << "(c" << i << j << " : s.f (" << i << ", " << j << ") = " << k << ") ";
-		}
-	}
-	cout << endl;
+	vector<vector<int>> s(9, vector<int>(9));
 
+	fora(i, 9) fora(j, 9) {
+		cin >> s[i][j];
+	}
+
+	cout << "import widget\n\nopen sudoku\n\ntheorem play (s : sudoku)";
+
+	fora(i, 9) fora (j, 9) {
+		if (s[i][j] == 0) continue;
+		cout << " (c" << i << j << " : s.f (" << i << ", " << j << ") = " << s[i][j] << ")";
+	}
+
+	solve(s, 0, 0);
+
+	cout << " :";
+
+	fora(i, 9) fora(j, 9) {
+		cout << " s.f (" << i << ", " << j << ") = " << s[i][j];
+		if (i != 8 || j != 8) cout << " ∧";
+	}
+
+	cout << " :=\nbegin [show_sudoku]\n\nend\n";
 }
